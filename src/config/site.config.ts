58bcee70550078356ecce6de07078c2f0a6167e5
rudrodip <official.rudrodipsarker@gmail.com>
2024-12-25 02:05:01 +0600

    site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,26 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Async Select",
+  title: "Async Select built with React & Shadcn UI",
+  description: "Generic async select for your next project (pun intended)",
+  origin: "https://async.rdsx.dev",
+  keywords: [
+    "Next.js 15",
+    "React",
+    "Shadcn UI",
+    "Async Select",
+    "Generic Selector",
+    "Async Select",
+    "Generic Select",
+  ],
+  og: "https://async.rdsx.dev/og.png",
+  creator: {
+    name: "rds_agi",
+    url: "https://rdsx.dev",
+  },
+  socials: {
+    github: "https://github.com/rudrodip/asyncr",
+    x: "https://x.com/rds_agi",
+  }
+}
