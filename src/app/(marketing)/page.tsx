7f70a4ef3103eaf22df346868ed2dcc68e339a94
Sachi Goyal <sachigoyal27@gmail.com>
2025-07-01 19:58:30 +0530

    changed x logo

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,16 +3,11 @@ import { siteConfig } from "@/config/site.config";
 import AsyncSelectExample from "@/components/examples/async-select-example";
 import AsyncSelectPreloadExample from "@/components/examples/async-select-preload-example";
 import { Button } from "@/components/ui/button";
-import {
-  CodeIcon,
-  GitHubLogoIcon,
-  TwitterLogoIcon,
-} from "@radix-ui/react-icons";
+import { CodeIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import { ComponentWrapper } from "@/components/wrapper";
 import { Docs } from "@/components/docs";
 
 export default function Home() {
-
   return (
     <main className="relative w-full flex flex-col min-h-screen px-2 md:px-0">
       <div className="w-full max-w-5xl mx-auto flex flex-col gap-4 mt-16 md:mt-24">
@@ -50,7 +45,8 @@ export default function Home() {
               slug="async-select-preload"
               Footer={() => (
                 <span className="text-xs text-muted-foreground">
-                  Async select component with preloaded options, with local filtering.
+                  Async select component with preloaded options, with local
+                  filtering.
                 </span>
               )}
             />
@@ -61,7 +57,7 @@ export default function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 function Socials() {
@@ -75,7 +71,13 @@ function Socials() {
         asChild
       >
         <a href={siteConfig.socials.x} target="_blank" rel="noreferrer">
-          <TwitterLogoIcon className="size-3 md:size-4" />
+          <svg
+            viewBox="0 0 1200 1227"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+          >
+            <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
+          </svg>
         </a>
       </Button>
       <Button
